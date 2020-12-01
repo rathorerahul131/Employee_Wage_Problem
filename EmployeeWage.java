5f2b4c9a8bb2675464c708f6212f5e1ecdaef8c3
neelrahulr <rathorerahul131@gmail.com>
2020-12-01 11:14:00 +0530

    Welcome User

---
+++ EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.project;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// print welcome message
+		System.out.println("Welcome to The Employee Wage System");
+
+	}
+
+}
